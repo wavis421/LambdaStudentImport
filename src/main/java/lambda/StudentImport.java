41c53ae53d5df63ae9a21cfb464ff3dfc353d721
wavis421 <wendy.avis@jointheleague.org>
2018-05-03 18:05:12 -0700

    Add import of courses from Pike13.

--- src/main/java/lambda/StudentImport.java
+++ src/main/java/lambda/StudentImport.java
@@ -7,8 +7,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import controller.GithubApi;
-import controller.StudentImportEngine;
 import controller.Pike13Api;
+import controller.StudentImportEngine;
 import model.MySqlDatabase;
 
 public class StudentImport {
@@ -17,11 +17,9 @@ public class StudentImport {
 
 	public String myHandler(Object input, Context context) {
 		// Import data starting 7 days ago
-		String startDateString = new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles")).minusDays(7)
-				.toString().substring(0, 10);
-
-		// Today (start time)
 		DateTime today = new DateTime().withZone(DateTimeZone.forID("America/Los_Angeles"));
+		String startDateString = today.minusDays(7).toString().substring(0, 10);
+		String courseEndDate = today.plusDays(120).toString().substring(0, 10);
 
 		LambdaLogger logger = context.getLogger();
 		logger.log(today.toString("yyyy-MM-dd HH:mm:ss") + " " + input + "\n");
@@ -35,7 +33,9 @@ public class StudentImport {
 			Pike13Api pike13Api = new Pike13Api(sqlDb, System.getenv("PIKE13_KEY"));
 			importer.importStudentsFromPike13(pike13Api);
 			importer.importAttendanceFromPike13(startDateString, pike13Api);
+//			importer.importCourseAttendanceFromPike13(startDateString, courseEndDate, pike13Api);
 			importer.importScheduleFromPike13(pike13Api);
+			importer.importCoursesFromPike13(pike13Api);
 
 			// Connect to github and import data
 			GithubApi githubApi = new GithubApi(sqlDb, System.getenv("GITHUB_KEY"));
